088d2419ca28b07d6a2478cd7209ad84fb28505f
Валерия Наумова <oliviia313@gmail.com>
2022-05-29 02:41:27 +0300

    first element

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import {count} from './numbers';
-import {length} from './numbers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-        	{'count * length = '}{count*length}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const firstElement = (<div>
+  Things To Do
+</div>);
 
-export default App;
+export { firstElement };
--- src/numbers.js
+++
@@ -1,5 +0,0 @@
-const count = 29;
-const length = 3;
-
-export {count};
-export {length};

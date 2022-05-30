b97deeca8ab93edfb7eea78a8a4d8055e9c98304
Валерия Наумова <oliviia313@gmail.com>
2022-05-30 22:45:08 +0300

    structure

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import ItemList from '../ItemList/ItemList';
+import Footer from '../Footer/Footer';
+import InputItem from '../InputItem/InputItem';
+
+const App = () => (<div>
+  <InputItem />
+  <h3>Things To Do</h3>
+  <ItemList />
+  <Footer />
+</div>);
+
+export default App;
---
+++ src/components/Item/Item.js
@@ -0,0 +1,3 @@
+const Item = () => (<span>Very important!</span>);
+
+export default Item;

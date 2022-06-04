1eda51f12e89a936ff965b2fb64c084a9674e6fd
Валерия Наумова <oliviia313@gmail.com>
2022-06-04 05:19:50 +0300

    input first styles

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,3 +1,15 @@
-const InputItem = () => (<div>InputItem</div>);
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
+const InputItem = () => <div>
+    <Box
+      sx={{
+        width: 500,
+        maxWidth: '100%',
+      }}
+    >
+      <TextField fullWidth label="Type in what you have to do..." id="fullWidth" />
+    </Box>
+</div>;
 
 export default InputItem;

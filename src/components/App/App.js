4f525be926688feb1c6d4e5e3da65502418f5388
Валерия Наумова <oliviia313@gmail.com>
2022-06-10 02:51:38 +0300

    App refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,35 @@
+import React from 'react';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css'
 
 
-const App = () => {
-  const items = [
-    {
-      value: 'Make a new app',
-      isDone: true
-    },
-    {
-      value: 'Learn props',
-      isDone: false
-    },
-    {
-      value: 'Do homework',
-      isDone: true
-    }
-  ];
-
-  return (
-    <div className={styles.wrap}>
-      <h3 className={styles.title}>Things To Do</h3>
-      <InputItem />
-      <ItemList items={items} />
-      <Footer count ={3} />
-    </div>);
-};
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: "Make a new app",
+        isDone: true
+      },
+      {
+        value: "Learn props",
+        isDone: false
+      },
+      {
+        value: "Do homework",
+        isDone: true
+      }
+    ];
+    return (
+      <div className={styles.wrap}>
+        <h3 className={styles.title}>Things To Do</h3>
+        <InputItem />
+        <ItemList items={items} />
+        <Footer count={3} />
+      </div>
+    );
+  }
+}
 
 export default App;

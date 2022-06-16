bb2285f20d35d618ab0a1070aa2830b1c40d4df9
Валерия Наумова <oliviia313@gmail.com>
2022-06-16 04:00:50 +0300

    item adding

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,7 +2,7 @@ import Item from '../Item/Item';
 import styles from './ItemList.module.css'
 
 const ItemList = ({ items, onClickDone, onClickDelete}) => (<ul className={styles.list}>
-  {items.map(item => <li key ={item.value}>
+  {items.map(item => <li key ={item.id}>
     <Item 
       value={item.value}
       isDone={item.isDone}
